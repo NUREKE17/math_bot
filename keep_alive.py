d0e98b6dba405dee4e6d871d769d9473c9239e28
NUREKE17 <kzdarhan721@gmail.com>
2024-12-07 17:07:11 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,5 +8,5 @@ def home():
     return "Бот 24/7 жұмыс істеп тұр!"
 
 def keep_alive():
-    t = Thread(target=lambda: app.run(host="0.0.0.0", port=5000))
+    t = Thread(target=lambda: app.run(host="0.0.0.0", port=4000))
     t.start()
